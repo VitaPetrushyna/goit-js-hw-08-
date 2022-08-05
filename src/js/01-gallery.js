b79d9e8592dad912bc336580390033ea5b67f031
VitaPetrushyna <masandra2018@ukr.net>
2022-08-05 21:15:49 +0300

    Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+const divGalleryEl = document.querySelector('.gallery');
+const cardsMarcup = createItemsGalleryMarcup(galleryItems);
+
+divGalleryEl.insertAdjacentHTML('beforeend', cardsMarcup);
+
+function createItemsGalleryMarcup(items) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    `;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
