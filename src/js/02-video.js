3f41a799c41c5e15dc456a8a2f9348d63b5729dd
VitaPetrushyna <masandra2018@ukr.net>
2022-08-06 20:53:04 +0300

    Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+}
+
+function startPlayback() {
+  if (JSON.parse(localStorage.getItem('videoplayer-current-time')) === null) {
+    return;
+  }
+  const paused = JSON.parse(localStorage.getItem('videoplayer-current-time'))[
+    'seconds'
+  ];
+  if (paused) {
+    player
+      .setCurrentTime(paused)
+      .then(function (seconds) {})
+      .catch(function (error) {
+        switch (error.name) {
+          case 'Error':
+            break;
+          default:
+            break;
+        }
+      });
+  }
+}
+startPlayback();
